4b5086d14129f32053f7749aa8057b3a9a797f78
Musthafa Faishal <musthafafaishal@gmail.com>
2022-10-08 16:13:42 +0700

    update

--- models/order.go
+++ models/order.go
@@ -4,7 +4,7 @@ import "time"
 
 type Order struct {
 	ID           uint      `gorm:"primaryKey;column:order_id" json:"-" validate:"required"`
-	OrderedAt    time.Time `gorm:"not null;autoCreateTime" json:"orderedAt,string,omitempty" validate:"required"`
+	OrderedAt    time.Time `gorm:"not null;autoCreateTime" json:"orderedAt" validate:"required"`
 	CustomerName string    `gorm:"type:VARCHAR(50)" form:"customerName" json:"customerName" validate:"required" binding:"required"`
 	Items        []Item    `form:"items" json:"items" validate:"required" binding:"required"`
 }
